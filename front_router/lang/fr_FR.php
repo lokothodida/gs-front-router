5f0ac4a4e40d82a6b595a37c74b3590ca5cb69f2
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2016-03-25 09:09:08 +0000

    Add french langauge file

---
+++ front_router/lang/fr_FR.php
@@ -0,0 +1,21 @@
+<?php
+
+// French (Google Translate)
+$i18n = array(
+  // plugin
+  'PLUGIN_NAME'    => 'Routeur Avant',
+  'PLUGIN_DESC'    => 'Créer des itinéraires pour les pages frontaux',
+  'MANAGE_ROUTES'  => 'Gérer les itinéraires',
+
+  // admin
+  'INIT_ERROR'            => 'Erreur d\'initialisation du plug-in!',
+  'MKDIR_ERROR'           => 'Impossible de créer le dossier %folder%',
+  'HTACESS_ERROR'         => 'Impossible de créer le fichier %htaccess%',
+  'INIT_ROUTE_ERROR'      => 'Impossible de créer le fichier des routes initiales',
+  'UPDATE_ROUTES_SUCCESS' => 'Itinéraires avec succès mis à jour',
+  'UPDATE_ROUTES_ERROR'   => 'Itinéraires de mise à jour d\'erreur',
+  'ROUTE'                 => 'Route',
+  'ACTION'                => 'Action',
+  'ADD_ROUTE'             => 'Ajouter Route',
+  'DELETE_ROUTE_SURE'     => 'Etes-vous sûr que vous voulez supprimer l\'itinéraire "%route%?"',
+);
