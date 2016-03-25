5fd9baacfc68c73b1c2bcd303ad405eb87b864b0
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2016-03-25 09:15:44 +0000

    Add russian language file

---
+++ front_router/lang/ru_RU.php
@@ -0,0 +1,21 @@
+<?php
+
+// Russian (Google Translate)
+$i18n = array(
+  // plugin
+  'PLUGIN_NAME'    => 'передний маршрутизатор',
+  'PLUGIN_DESC'    => 'Создание интерфейсных страниц маршруты маршруты',
+  'MANAGE_ROUTES'  => 'Управление маршрутов',
+
+  // admin
+  'INIT_ERROR'            => 'Ошибка инициализации плагина!',
+  'MKDIR_ERROR'           => 'Не удалось создать %folder% папку',
+  'HTACESS_ERROR'         => 'Не удалось создать %htaccess% файл',
+  'INIT_ROUTE_ERROR'      => 'Не удалось создать начальные маршруты файл',
+  'UPDATE_ROUTES_SUCCESS' => 'Успешно обновленные маршруты',
+  'UPDATE_ROUTES_ERROR'   => 'Ошибка обновления маршрутов',
+  'ROUTE'                 => 'маршрут',
+  'ACTION'                => 'действие',
+  'ADD_ROUTE'             => 'Добавить маршрут',
+  'DELETE_ROUTE_SURE'     => 'Вы уверены, что хотите удалить маршрут "%route%?"',
+);
