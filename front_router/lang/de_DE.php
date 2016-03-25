28722df3acb0beeafce587b794aa4a2a7d82ee73
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2016-03-25 09:20:57 +0000

    Add german language file

---
+++ front_router/lang/de_DE.php
@@ -0,0 +1,21 @@
+<?php
+
+// German (Google Translate)
+$i18n = array(
+  // plugin
+  'PLUGIN_NAME'    => 'Vorne Router',
+  'PLUGIN_DESC'    => 'Erstellen Front-End-Routen Seite Routen',
+  'MANAGE_ROUTES'  => 'Routen verwalten',
+
+  // admin
+  'INIT_ERROR'            => 'Fehler das Plugin initialisiert!',
+  'MKDIR_ERROR'           => 'Konnte den %folder% Ordner erstellen',
+  'HTACESS_ERROR'         => 'Konnte die %htaccess% Datei erstellen',
+  'INIT_ROUTE_ERROR'      => 'Konnte nicht anfänglichen Routen erstellen Datei',
+  'UPDATE_ROUTES_SUCCESS' => 'Erfolgreich aktualisiert Routen',
+  'UPDATE_ROUTES_ERROR'   => 'Fehler beim Aktualisieren der Routen',
+  'ROUTE'                 => 'Route',
+  'ACTION'                => 'Vorgehen',
+  'ADD_ROUTE'             => 'Route hinzufügen',
+  'DELETE_ROUTE_SURE'     => 'Sind Sie sicher, dass Sie die Route zu löschen "%route%?"',
+);
