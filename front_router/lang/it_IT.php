5bff0f35b635e7489f9028d734d96ac24eab15de
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2016-03-25 09:28:52 +0000

    Add italian language file

---
+++ front_router/lang/it_IT.php
@@ -0,0 +1,21 @@
+<?php
+
+// Italian (Google Translate)
+$i18n = array(
+  // plugin
+  'PLUGIN_NAME'    => 'Frontale del router',
+  'PLUGIN_DESC'    => 'Crea front-end percorsi page Itinerari',
+  'MANAGE_ROUTES'  => 'Gestisci Percorsi',
+
+  // admin
+  'INIT_ERROR'            => 'Errore durante l\'inizializzazione del plugin!',
+  'MKDIR_ERROR'           => 'Impossibile creare la cartella %folder%',
+  'HTACESS_ERROR'         => 'Impossibile creare il file %htaccess%',
+  'INIT_ROUTE_ERROR'      => 'Impossibile creare il file percorsi iniziali',
+  'UPDATE_ROUTES_SUCCESS' => 'Aggiornato con successo percorsi',
+  'UPDATE_ROUTES_ERROR'   => 'Errore percorsi di aggiornamento',
+  'ROUTE'                 => 'Route',
+  'ACTION'                => 'Azione',
+  'ADD_ROUTE'             => 'Aggiungi Strada',
+  'DELETE_ROUTE_SURE'     => 'Sei sicuro di voler eliminare il percorso "%route%?"',
+);
